2bedaec75dbdbfcc4507cb7c0ad2a879f8231d69
sara luongo <saraluongo202002@gmail.com>
2026-04-10 17:47:25 +0200

    fatto ciclo for che scorre array di oggetti e con template literals vado ad accedere ai valori e stamparli nel div html contenitore card, inizio a modificare lo stile

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
-'use strict'
+'use strict';
+const contenitoreCard = document.querySelector('.card-container');
+
+
+let containerHtml = ''
+
+for (let i = 0; i < teamMembers.length; i++) {
+    const member = teamMembers[i];
+    const tempContainerHtml = ` 
+        <div id="card">
+            <div class="card-img">
+                    <img src="${member.img}" alt="${member.name}">
+            </div>
+            <div class="info">
+                    <h3>${member.name}</h3>
+                    <p>${member.role}</p>
+                    <p>${member.email}</p>
+            </div>
+        </div>
+    `
+    containerHtml += tempContainerHtml
+}
+
+contenitoreCard.innerHTML = containerHtml
